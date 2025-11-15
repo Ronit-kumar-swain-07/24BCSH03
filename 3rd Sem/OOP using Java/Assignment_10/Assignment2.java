f46ce21d8efdbb2518b80371f92b9728f084f82d
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2025-11-15 11:03:20 +0530

    commit

---
+++ 3rd Sem/OOP using Java/Assignment_10/Assignment2.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Assignment2 extends Frame {
+    public TextField tf1, tf2, RF;
+    public Button b;
+
+    public Assignment2() { // constructor
+        //Frame
+        setTitle("Assignment-2");
+        setSize(300, 250);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        setLayout(null);
+        // Text fields and Button
+        tf1 = new TextField();
+        tf2 = new TextField();
+        RF = new TextField();
+
+        RF.setEditable(false); // Result field Should not be editable
+        b = new Button("Subtract");
+
+        // Bounds for the components
+        tf1.setBounds(30, 50, 130, 30);
+        tf2.setBounds(30, 100, 130, 30);
+        RF.setBounds(30, 150, 130, 30);
+        b.setBounds(30, 200, 130, 30);
+
+        // Adding components to the frame
+        add(tf1);
+        add(tf2);
+        add(RF);
+        add(b);
+
+        // Action listener for the button
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int num1 = Integer.parseInt(tf1.getText());
+                    int num2 = Integer.parseInt(tf2.getText());
+                    int result = num1 - num2;
+                    RF.setText(Integer.toString(result));
+                } catch (NumberFormatException ex) {
+                    RF.setText("Invalid Input");
+                }
+            }
+        });
+    }
+    public static void main(String[] args) {
+        Assignment2 frame = new Assignment2();
+        frame.setVisible(true);
+    }
+}
