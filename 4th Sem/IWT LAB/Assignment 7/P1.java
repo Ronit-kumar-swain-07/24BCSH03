61a2ad2399db1d820c3ff1391d79b27d0ef8873e
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2026-03-24 21:44:00 +0530

    commit

--- 4th Sem/IWT LAB/Assignment 7/P1.java
+++ 4th Sem/IWT LAB/Assignment 7/P1.java
@@ -1,3 +1,24 @@
-public class P1 {
-    
+// 1.	Write a Java program to establish a connection with an RDBMS.
+
+package jpack;
+import java.sql.*;
+public class  P1{
+	public static void main(String[] args) {
+		try {
+			String driver = "org.postgresql.Driver";
+			String url = "jdbc:postgresql://192.168.1.17/cse_db24";
+			String username = "24bcsh03";
+			String password = "24bcsh03";
+			
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url,username,password);
+			
+			if(con != null) {
+				System.out.println("Connected........");
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 }
