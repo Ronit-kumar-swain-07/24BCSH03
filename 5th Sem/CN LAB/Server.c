7701a34e3c58c47c8a85abd2f37182b10f4810bb
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2026-08-27 20:15:59 +0530

    Commit

--- 5th Sem/CN LAB/Server.c
+++ 5th Sem/CN LAB/Server.c
@@ -3,8 +3,8 @@
 #include<string.h>
 #include<unistd.h>
 #include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 void error(char *msg)
 {
 	perror(msg);
